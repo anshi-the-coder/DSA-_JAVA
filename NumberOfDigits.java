76fb50e1ba5606c7a670972267819bca88c25bd1
Anshika Gangwar <anshikagangwar341@gmail.com>
2025-03-14 21:45:54 +0530

    Number of Digits

---
+++ NumberOfDigits.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class NumberOfDigits{
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int node= 0;
+        int temp=n;
+        while(temp!=0){
+            temp=temp/10;
+            node++;
+        }
+        int div = (int)Math.pow(10,node-1);
+        while(n!=0){
+            int q=n/div;
+            System.out.println(q);
+
+            n= n%10;
+            div= div/10;
+        }
+    }
+}

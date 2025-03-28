eb56822da4c753e31b14c49d36acd5bff53958af
Anshika Gangwar <anshikagangwar341@gmail.com>
2025-03-28 22:30:32 +0530

    Pattern 14

---
+++ Pattern_14.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Pattern_14{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        for(int i=1;i<=10;i++){
+            int v=x*i;
+            System.out.println(x + "*" + i + " = " + v);
+        }
+    }
+}

b78adbd4257e4d8993ba3f54c0080373aa8f49f8
anshi-the-coder <anshikagangwar341@gmail.com>
2025-07-31 21:39:42 +0530

    Inverted Half Pyramid(Number)

--- InvertedHalfPyramid.java
+++ InvertedHalfPyramid.java
@@ -6,16 +6,12 @@
 import java.util.*;
 public class InvertedHalfPyramid{
     public static void main(String args[]){
-        int n=4;
+        int n=5;
         //outer loop
         for(int i=1;i<=n;i++){
             //inner loop -> space print
-            for(int j=1;j<=n-i;j++){
-                System.out.print(" ");
-            }
-            //inner loop ->start print
             for(int j=1;j<=i;j++){
-                System.out.print("*");
+                System.out.print(j+" ");
             }
             System.out.println();
         }
